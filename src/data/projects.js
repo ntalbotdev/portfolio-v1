dd21592491ed00ba5fffe11f04e432b9838985da
Nicolas Talbot <talbotn123@gmail.com>
2024-08-24 04:28:22 -0400

    Corrected url for github repo

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,7 @@ export default {
 				"Built with Vue.js and hosted on Cloudflare Pages, my personal portfolio showcases my skills and experience in crafting modular, reusable components and efficient state management. This portfolio features a contact form that integrates with the EmailJS library, demonstrating my ability to work with third-party APIs.",
 			imageUrl: "/images/projects/portfolio.webp",
 			siteUrl: "https://ntalbot.dev",
-			githubUrl: "https://github.com/#",
+			githubUrl: "https://github.com/ntalbotdev/portfolio-v1",
 		},
 	],
 	fr: [
@@ -18,7 +18,7 @@ export default {
 				"Conçu avec Vue.js et hébergé sur Cloudflare Pages, mon portfolio personnel met en valeur mes compétences et mon expérience dans la création de composants modulaires et réutilisables ainsi que dans la gestion efficace des états. Ce portfolio présente un formulaire de contact qui s'intègre à la bibliothèque EmailJS, démontrant ma capacité à travailler avec des API tierces.",
 			imageUrl: "/images/projects/portfolio.webp",
 			siteUrl: "https://ntalbot.dev",
-			githubUrl: "https://github.com/#",
+			githubUrl: "https://github.com/ntalbotdev/portfolio-v1",
 		},
 	],
 }
