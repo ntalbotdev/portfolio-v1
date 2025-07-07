86be5ff2e5c1355fcd8f03df0a985cc34a921ce3
Nicolas Talbot <talbotn123@gmail.com>
2025-07-07 04:13:53 -0400

    Update footer copyright year to be dynamic

--- src/views/TheFooter.vue
+++ src/views/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="footer">
-    <p class="footer__text">&copy; 2024 Nicolas Talbot. {{  $t("footerRights") }}</p>
+    <p class="footer__text">&copy; {{ new Date().getFullYear() }} Nicolas Talbot. {{  $t("footerRights") }}</p>
   </footer>
 </template>
 
