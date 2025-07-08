fe9da1829e17adf20e2a8dd9887c6efa3e9e48f1
Nicolas Talbot <talbotn123@gmail.com>
2025-07-08 02:02:36 -0400

    Update project tags in projects.js for consistency by replacing "Vue.js" with "Vue"

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,18 +2,18 @@ export default {
 	en: [
 		{
 			title: "Portfolio",
-			tags: ["Vue.js", "JS", "SCSS"],
+			tags: ["Vue", "JS", "SCSS"],
 			description:
-				"Built with Vue.js and hosted on Cloudflare Pages, my personal portfolio showcases my skills and experience in crafting modular, reusable components and efficient state management. This portfolio features a contact form that integrates with the EmailJS library, demonstrating my ability to work with third-party APIs.",
+				"Built with Vue and hosted on Cloudflare Pages, my personal portfolio showcases my skills and experience in crafting modular, reusable components and efficient state management. This portfolio features a contact form that integrates with the EmailJS library, demonstrating my ability to work with third-party APIs.",
 			imageUrl: "/images/projects/portfolio.webp",
 			siteUrl: "https://ntalbot.com",
 			githubUrl: "https://github.com/ntalbotdev/portfolio-v1",
 		},
 		{
 			title: "SpaceX clone",
-			tags: ["Vue.js", "JS", "SCSS"],
+			tags: ["Vue", "JS", "SCSS"],
 			description:
-				"This is a SpaceX homepage clone, developed using Vue.js. By closely replicating the design, I demonstrate my ability to translate design concepts into functional web applications, leveraging modern front-end technologies to deliver a polished and responsive experience.",
+				"This is a SpaceX homepage clone, developed using Vue. By closely replicating the design, I demonstrate my ability to translate design concepts into functional web applications, leveraging modern front-end technologies to deliver a polished and responsive experience.",
 			imageUrl: "/images/projects/spacex-clone.webp",
 			siteUrl: "https://spacex-clone-ntalbotdev.vercel.app",
 			githubUrl: "https://github.com/ntalbotdev/spacex-clone",
@@ -30,18 +30,18 @@ export default {
 	fr: [
 		{
 			title: "Portfolio",
-			tags: ["Vue.js", "JS", "SCSS"],
+			tags: ["Vue", "JS", "SCSS"],
 			description:
-				"Conçu avec Vue.js et hébergé sur Cloudflare Pages, mon portfolio personnel met en valeur mes compétences et mon expérience dans la création de composants modulaires et réutilisables ainsi que dans la gestion efficace des états. Ce portfolio présente un formulaire de contact qui s'intègre à la bibliothèque EmailJS, démontrant ma capacité à travailler avec des API tierces.",
+				"Conçu avec Vue et hébergé sur Cloudflare Pages, mon portfolio personnel met en valeur mes compétences et mon expérience dans la création de composants modulaires et réutilisables ainsi que dans la gestion efficace des états. Ce portfolio présente un formulaire de contact qui s'intègre à la bibliothèque EmailJS, démontrant ma capacité à travailler avec des API tierces.",
 			imageUrl: "/images/projects/portfolio.webp",
 			siteUrl: "https://ntalbot.com",
 			githubUrl: "https://github.com/ntalbotdev/portfolio-v1",
 		},
 		{
 			title: "SpaceX clone",
-			tags: ["Vue.js", "JS", "SCSS"],
+			tags: ["Vue", "JS", "SCSS"],
 			description:
-				"Ceci est une réplique de la page d'accueil de SpaceX, développée avec Vue.js. En reproduisant le design, je démontre ma capacité à transposer les concepts de design en applications web fonctionnelles, en exploitant les dernières technologies front-end pour offrir une expérience raffinée et réactive.",
+				"Ceci est une réplique de la page d'accueil de SpaceX, développée avec Vue. En reproduisant le design, je démontre ma capacité à transposer les concepts de design en applications web fonctionnelles, en exploitant les dernières technologies front-end pour offrir une expérience raffinée et réactive.",
 			imageUrl: "/images/projects/spacex-clone.webp",
 			siteUrl: "https://spacex-clone-ntalbotdev.vercel.app",
 			githubUrl: "https://github.com/ntalbotdev/spacex-clone",
