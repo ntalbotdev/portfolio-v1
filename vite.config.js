f7c5d75a15580ddb23e97cb0ad816f44e03794b2
Nicolas Talbot <talbotn123@gmail.com>
2025-07-29 05:05:45 -0400

    chore: add Tailwind CSS support with Vite integration
    - Updated package.json to include Tailwind CSS and its dependencies.
    - Configured Vite to use Tailwind CSS as a plugin.

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,15 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
 
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
@@ -22,5 +23,5 @@ export default defineConfig({
         additionalData: `@use "@/assets/variables" as *;`
       }
     }
-  }
+  },
 })
