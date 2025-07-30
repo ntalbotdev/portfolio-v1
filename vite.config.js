1fe5e980c4b6b8dc05f8a59dfe9a471208d3a7b8
Nicolas Talbot <talbotn123@gmail.com>
2025-07-30 03:56:20 -0400

    feat: remove SCSS preprocessor options from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -16,12 +16,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/assets/variables" as *;`
-      }
-    }
-  },
+  }
 })
